456a2deb087e938cbf895bceda4cb9b9744dc803
William Santos <williamsantos@localhost.localdomain>
2016-11-13 09:45:10 -0300

    Setting Topology

--- topology.py
+++ topology.py
@@ -1,3 +1,15 @@
 # Topologies
 
-Class Topology:
+class Topology:
+    def __init__(self):
+        self.Nodes = set()
+        self.Links = default(list)
+        self.Lengths = {}
+
+    def add_Node(self, NodeID):
+        self.Nodes.add(value)
+
+    def add_Link(self, Origin, Destination, Length):
+        self.Links[Origin].append(Destination)
+        self.Links[Destination].append(Origin)
+        self.Lengths[(Origin, Destination)] = Length
