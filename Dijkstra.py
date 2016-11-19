fa27207d88226e4bf4ad71980ef0ee37d1f0a1ae
William Santos <williamsantos@localhost.localdomain>
2016-11-19 18:50:43 -0300

    At least run, but still is wrong

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,17 +1,34 @@
 # Dijkstra Algorithm for Optical Networks
 
 from Topology import Topology
+from operator import itemgetter
 
 class Dijkstra:
     def __init__(self, Origin, Destination):
         T = Topology()
-        Route = []
-        TotalWeight = 0
+        self.Route = []
+        VisitedNodes = []
+        CurrentNode = Origin
 
-        for CurrentNode in T.Nodes:
-            MinorWeight = []
+        while T.Nodes:
+            MinorLength = []
             for CurrentLink in T.Links:
                 if CurrentLink[0] == CurrentNode:
-                    MinorWeight.append(CurrentLink[2])
-            MinorWeight.sort()
-            TotalWeight += MinorWeight[0]
+                    MinorLength.append(CurrentLink)
+
+            MinorLength = sorted(MinorLength, key=itemgetter(2))
+
+            while True:
+                if MinorLength[0][1] in VisitedNodes:
+                    del MinorLength[0]
+                else:
+                    VisitedNodes.append(MinorLength[0][0])
+                    self.Route.append(MinorLength[0])
+                    CurrentNode = self.Route[-1][1]
+                    break
+
+            if CurrentNode == Destination:
+                break
+
+    def getRoute(self):
+        return self.Route
--- Initialize.py
+++ Initialize.py
@@ -1,10 +1,10 @@
 # Implements Dijsktra's Algorithm
 
 from Dijkstra import Dijkstra
-# from Topology import Topology
 
 Origin = input("-> Origin's Node: ")
-Destination = input("-> Destination's Node:")
+Destination = input("-> Destination's Node: ")
 
 Route = Dijkstra(Origin, Destination)
-print(Route)
+
+print(Route.getRoute())
